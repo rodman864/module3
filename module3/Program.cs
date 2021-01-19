6ffd10fcde6d3df3bf26e1ad3af3d1956d586293
Rodrigus Mckinney <rodrigusmckinney@gmail.com>
2021-01-19 14:31:10 -0500

    Module 3

---
+++ module3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace module3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cake<string> Cake = new Cake<string>();
+
+            Cake.type = "Wedding Cake";
+            Cake.flavor = "Vanilla";
+
+            Cake<string> Cake1 = new Cake<string>();
+            Cake1.type = "Birthday Cake";
+            Cake1.flavor = "Chocolate";
+
+            Console.WriteLine("My favorite cake is" + Cake.type
+                     + "with the flavor of" + Cake.flavor + "I don't like my" + Cake1.type
+                     + "it has the flavor of" + Cake.flavor);
+                
+
+
+
+
+            
+        }
+    }
+}
+public class Cake<T>
+{
+    public T type { get; set; }
+    public T flavor { get; set; }
+
+
+}
